a2e6d7d7780174c03619dc57eaaa0623a58962a2
smolkin <daniel.smolkin@gmail.com>
2014-02-16 14:27:06 -0700

    First commit. Still need to figure out how to edit the master tex file

---
+++ mathlog.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+date_formatted=$(date +%Y%m%d)
+pathname=~/Math/mathlog/
+
+while getopts ":m:vd:" opt; do
+    case $opt in 
+        v) evince ~/Math/mathlog/mathlog.pdf &
+            exit 0 ;;
+        m) if [ ! -e $pathname$date_formatted".tex" ]
+        then
+            # append \input whatever to mathlog.tex
+        fi
+
+        echo $OPTARG >> $pathname$date_formated".tex"
+        exit 0 ;;
+        d) date_formatted=$OPTARG ;;
+        \?)  echo "Invalid option -$OPTARG" >&2
+            exit 1 ;;
+    esac 
+
+    if [ ! -e $pathname$date_formatted".tex" ]
+    then
+        # append \input whatever to mathlog.tex
+    fi
+
+    vim $pathname$date_formatted".tex"
