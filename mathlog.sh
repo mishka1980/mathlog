50640c294b08dcff6033c7e4944b999b8d1241f1
smolkin <daniel.smolkin@gmail.com>
2014-07-05 23:18:25 -0600

    Added option to compile, moved -d to start of case switch thing, set section name in updateMaster

--- mathlog.sh
+++ mathlog.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 
 date_formatted=$(date +%Y%m%d)
-sect_name=$(date +%B\ %d,\ %Y)
-pathname=~/Math/mathlog/
+pathname=/home/ds/Math/mathlog
 
 function updateMaster {
-    sed -i "s/\%\%\%/\n\\\section{$sect_name}\n\\\input{$date_formatted.tex}\n\%\%\%/g" '/home/ds/Math/mathlog.tex'
+    sect_name=$(date -d$date_formatted +%B\ %d,\ %Y) || exit 1
+    sed -i "s/\%\%\%/\n\\\section{$sect_name}\n\\\input{$date_formatted.tex}\n\%\%\%/g" "$pathname/mathlog.tex"
+    # to change: use absolute filepath. sed is begin finnicky however.
 }
 
-
-while getopts ":m:vd:" opt; do
+while getopts ":m:vd:c" opt; do
     case $opt in 
+        d) date_formatted=$OPTARG ;;
+        # set the date of your journal entry to a given date.
         v) evince ~/Math/mathlog/mathlog.pdf &
             exit 0 ;;
-        m) if [ ! -e $pathname$date_formatted".tex" ]
+            # view the pdf
+        m) if [ ! -e $pathname/$date_formatted'.tex' ]
         then
             updateMaster
         fi
 
-        echo $OPTARG >> $pathname$date_formated".tex"
+        echo $OPTARG >> $pathname/$date_formated'.tex'
         exit 0 ;;
-        d) date_formatted=$OPTARG ;;
+        # write a short journal entry without opening a text editor
+        c) pdflatex -output-directory=$pathname mathlog.tex 
+            exit 0 ;;
+        # compiles
         \?)  echo "Invalid option -$OPTARG" >&2
             exit 1 ;;
     esac 
 done 
 
-if [ ! -e $pathname$date_formatted".tex" ]
+if [ ! -e $pathname/$date_formatted'.tex' ]
 then
     updateMaster
 fi
 
-vim $pathname$date_formatted".tex"
+vim $pathname/$date_formatted'.tex'
